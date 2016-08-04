f0cf7efaba7baaca59e48587a33f735c03929139
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-04 12:07:52 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {:name => "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {:name => "Sailboat"}).uniq
+  end
+
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: {:name => "Motorboat"}).uniq
+  end
+
+  def self.talented_seamen
+    # includes(boats: :classifications).where({:classifications => {:name => "Motorboat"}} && {:classifications => {:name =>'Sailboat'}})
+    t_s = sailors & motorboat_operators
+    where(id: t_s.map(&:id))
+  end
+
+  def self.non_sailors
+    # includes(boats: :classifications).except(classifications: {:name => "Sailboat"})
+    n_s = all - sailors
+    where(id: n_s.map(&:id))
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.longest
+    joins(:boats).order('length DESC').limit(2)
+  end
+
+  def self.my_all
+    uniq
+  end
 end
